a294837d0dd175b339207ca54522c39f6f2ea4de
ClizaMoras <morascliza@gmail.com>
2026-08-23 16:01:38 +0530

    Prepare Django project for deployment

--- lm_project/settings.py
+++ lm_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-development-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
